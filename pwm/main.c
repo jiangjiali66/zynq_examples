b45e3a895ac95e750e043ebcc62cc7773a7ef684
jiangjiali66 <jiangjiali66@gmail.com>
2015-08-12 02:44:35 -0400

    axi-gpio pwm

---
+++ pwm/main.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/10/2015 06:02:36 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jiang JiaJi (@.@), jiangjiali666@gmail.com
+ *        Company:  Xilinx XUP
+ *
+ * =====================================================================================
+ */
+
+#include "../xlib/xil_pwm.h"
+#define PWM_BASE_ADDR 0x43C00000
+
+int main()
+{
+    xil_pwm* mypwm = XilPwmCreate(PWM_BASE_ADDR);
+    mypwm->setPeriod(mypwm, 0, 10000);
+    mypwm->setDuty(mypwm, 0, 20);
+    
+    mypwm->setPeriod(mypwm, 1, 10000);
+    mypwm->setDuty(mypwm, 1, 40);
+
+    mypwm->setPeriod(mypwm, 2, 10000);
+    mypwm->setDuty(mypwm, 2, 60);
+
+    mypwm->setPeriod(mypwm, 3, 10000);
+    mypwm->setDuty(mypwm, 3, 80);
+
+    printf("pwm0 Duty is %f\n", mypwm->getDuty(mypwm, 0));
+    
+    XilPwmDestory(mypwm);
+    
+    return 0;
+}
---
+++ xlib/xil_io.h
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  xil_io.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/10/2015 03:17:54 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jiang JiaJi (@.@), jiangjiali666@gmail.com
+ *        Company:  Xilinx XUP
+ *
+ * =====================================================================================
+ */
+
+#ifndef _XIL_IO_H_
+#define _XIL_IO_H_
+#include "xil_types.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <termios.h>
+#include <errno.h>
+
+typedef struct _xil_io xil_io;
+
+typedef struct _xil_io{
+    volatile unsigned long *io;
+    u8 (*Xil_In8)(xil_io *me, u32 RegOffset);
+    u16 (*Xil_In16)(xil_io *me, u32 RegOffset);
+    u32 (*Xil_In32)(xil_io *me, u32 RegOffset);
+
+    void (*Xil_Out8)(xil_io *me, u32 RegOffset, u8 Value);
+    void (*Xil_Out16)(xil_io *me, u32 RegOffset, u16 Value);
+    void (*Xil_Out32)(xil_io *me, u32 RegOffset, u32 Value);
+}xil_io;
+
+xil_io *XilIOCreate(u32 Addr);
+void XilIODestory(xil_io *me);
+
+#endif
