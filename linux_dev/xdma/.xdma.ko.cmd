d8d12ec20ee354b41bafea45d662d99a01b50d70
jiangjiali66 <jiangjiali66@gmail.com>
2015-08-26 16:20:03 +0800

    忘记更新了什么

--- linux_dev/xdma/.xdma.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/jjl/nfs/zynq_examples/linux_dev/xdma/xdma.ko := arm-xilinx-linux-gnueabi-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/jjl/nfs/zynq_examples/linux_dev/xdma/xdma.ko /home/jjl/nfs/zynq_examples/linux_dev/xdma/xdma.o /home/jjl/nfs/zynq_examples/linux_dev/xdma/xdma.mod.o
